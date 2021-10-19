c4909492e8562365e04767b842f5459218bb81f8
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-19 14:22:08 +0200

    auctions routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+
 import { app } from "../app";
 import {routes as messages} from './messages'
+import {routes as auctions} from './auctions'
+import {routes as accounts} from './accounts'
 
 let router = express.Router();
 
 app.use('/messages', messages);
+app.use('/auctions', auctions);
+app.use('/accounts', accounts);
 
 export default router;
