18ab4d6f9b9d15ec2fef3abb84a5d3926ecfaf8a
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-19 10:42:32 +0200

    start of refactor`

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,16 @@
 import express, { Application } from "express";
+import morgan from "morgan";
+import Router from "./routes";
 
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
 
-app.get("/ping", async (_req, res) => {
-  res.send({
-    message: "pong3",
-  });
-});
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+app.use(Router);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
