ea52f2497e4b1014ccd4d2d7087b0c6ebcfece88
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-20 15:24:02 +0200

    pass information to controller

--- api/src/routes/accounts/post.ts
+++ api/src/routes/accounts/post.ts
@@ -1,10 +1,11 @@
 import express from "express";
+
+import { createUser } from "../../controllers/user.controller";
+
 var router = express.Router();
 
 router.post('/', async (req, res) => {
-  console.log(req);
-
-  res.send(req.body);
+  res.send( await createUser(req.body) );
 })
 
 export {router}
