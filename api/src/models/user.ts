7e3233cd103be538036def9f94526c0d340be9ef
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-20 13:40:55 +0200

    user the most iconic model

---
+++ api/src/models/user.ts
@@ -0,0 +1,32 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from "typeorm";
+  
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  
+  @Column()
+  firstName!: string;
+  
+  @Column()
+  lastName!: string;
+  
+  @Column()
+  email!: string;
+
+  @Column()
+  password!: string;
+  
+  @CreateDateColumn()
+  createdAt!: Date;
+  
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
+  
