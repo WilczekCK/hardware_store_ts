bc06145ba8a96bce79c054dbbe18932257e8d6cc
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-21 16:10:54 +0200

    trying to achieve best mysql queries

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -8,11 +8,33 @@ export interface UserPayload {
     password: string;
 }
 
+export interface userFilters {
+    where?: Record<string, number> | string;
+    limit?: number;
+    order?: Record<string, number> | string;
+}
+
 export const getAllUsers = async(): Promise<Array<any>> => {
     const userRepository = getRepository(User);
 
     return userRepository.find({});
 }
+
+export const getUsers = async(payload: userFilters): Promise<Array<any>> => {
+    const userRepository = getRepository(User);
+
+    const {where, take, order} = {
+        where: payload.where ? payload.where : {},
+        take:  payload.limit ? payload.limit : 5,
+        order: payload.order ? payload.order : { "id": "DESC" },
+    }
+
+    return userRepository.find( {
+        where,
+        take,
+    });
+}
+
 
 export const createUser = async(payload: UserPayload): Promise<User> => {
     const userRepository = getRepository(User);
--- api/src/routes/accounts/get.ts
+++ api/src/routes/accounts/get.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getAllUsers } from "../../controllers/user.controller";
+import { getAllUsers, getUsers } from "../../controllers/user.controller";
 
 var router = express.Router();
 
@@ -8,8 +8,15 @@ router.get('/', async (req, res) => {
   res.send( await getAllUsers() );
 })
 
-router.get('/:id', function (req, res) {
-  res.send(`Get account with ID ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+  
+  res.send(
+    await getUsers( { 
+      where: { id: parseInt(req.params.id) }, 
+      ...req.body 
+    })
+  );
+
 })
 
 export {router}
