6d410e4a5359360976b5af2457beea4699b762c4
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-21 16:25:44 +0200

    better route for get

--- api/src/routes/accounts/get.ts
+++ api/src/routes/accounts/get.ts
@@ -5,18 +5,18 @@ import { getAllUsers, getUsers } from "../../controllers/user.controller";
 var router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.send( await getAllUsers() );
+  res.send(
+    await getUsers( req.body )
+  );
 })
 
 router.get('/:id', async (req, res) => {
-  
   res.send(
     await getUsers( { 
       where: { id: parseInt(req.params.id) }, 
       ...req.body 
     })
   );
-
 })
 
 export {router}
