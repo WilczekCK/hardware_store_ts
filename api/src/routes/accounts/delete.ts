0fc8a8a4324d2793cbbc63e9a3de26f47ab69878
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-23 20:45:06 +0200

    route prepared

--- api/src/routes/accounts/delete.ts
+++ api/src/routes/accounts/delete.ts
@@ -1,17 +1,19 @@
 import express from "express";
+import { DeleteResult } from "typeorm";
 
 import { removeUsers } from "../../controllers/user.controller";
 
 var router = express.Router();
 
 router.delete('/', async (req, res) => {  
-  const { where } = req.body;
+  const { where } = req.body; // () => { id: [number] }
+  let { affected, raw } :any = await removeUsers( { where } ) // () => results how many deleted
 
-  res.send(
-    await removeUsers( { 
-      where
-    })
-  );
+  if ( affected > 0 ) {
+    res.send( {status: 200, message: `${affected} results removed, all ok`} );
+  }
+
+  res.send( {status: 202, message: `No results`} );
 })
 
 export {router}
