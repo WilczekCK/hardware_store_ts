aba6e85abed3b9742f6ee40468fd8a84262978d2
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-11-24 14:15:42 +0100

    delete with id in url

--- api/src/routes/accounts/delete.ts
+++ api/src/routes/accounts/delete.ts
@@ -14,4 +14,14 @@ router.delete('/', async (req, res) => {
   res.send( {status: 202, message: `No results`} );
 })
 
+router.delete('/:id', async (req, res) => {  
+  const { affected, raw } :any = await removeUsers( { where: [{id: req.params.id}]} ) // () => results how many deleted
+
+  if ( affected > 0 ) {
+    return res.send( {status: 200, message: `${affected} results removed, all ok`} );
+  }
+
+  res.send( {status: 202, message: `No results`} );
+})
+
 export {router}
