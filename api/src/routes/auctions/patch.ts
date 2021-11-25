17623b763fda83b3c6c5492715229cd81e8ccb02
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-11-25 13:34:05 +0100

    put/patch routes

--- api/src/routes/auctions/patch.ts
+++ api/src/routes/auctions/patch.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import { modifyAuctions } from "../../controllers/auction.controller";
+
 var router = express.Router();
 
-router.patch('/', function (req, res) {
-  res.send('Update all auctions from inbox')
-})
+router.patch('/', async (req, res) => {
+  const { where, set } = req.body;
+  const { affected, raw } :any = await modifyAuctions( { where, set } ) // () => results how many changed
 
-router.patch('/:id', function (req, res) {
-  res.send(`Update auctions with ID ${req.params.id}`);
+  if ( affected > 0 ) {
+    return res.send( {status: 200, message: `${affected} results modified, all ok`} );
+  }
+
+  res.send( {status: 202, message: `No results`} );
 })
 
 export {router}
