aa95a7ffc62d56c20c1303d051d816380a141969
WilczekCK <pwilk0108@gmail.com>
2022-01-24 13:59:06 +0100

    installed bcrypt module

--- api/src/controllers/hasing.controller.ts
+++ api/src/controllers/hasing.controller.ts
@@ -1,1 +1,6 @@
 import { Connection, DeleteResult, getConnection, getRepository, UpdateResult } from "typeorm";
+import { genSalt, hash, compare } from "bcrypt";
+
+export const hashPassword = async (password: string): Promise<string> => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+}
