5eb4f4bebe4e6e058765f2f2d3366270a3860fcc
WilczekCK <pwilk0108@gmail.com>
2022-01-27 16:09:25 +0100

    generateVerificationString for email verify

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -24,9 +24,16 @@ export const areCredentialsValid = async ({where: whereQuery}: queryResults): Pr
     return true;
 }
 
+export const generateVerificationString = (): string => {
+    const letterArray: string[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+    return letterArray.sort(() => 0.5 - Math.random()).join("");
+}
+
 export const sendVerificationEmail = async (): Promise<boolean> => {
     const transporter = createTransport(config)
     const mailSent = await transporter.sendMail(mails.verification);
+
+    console.log(generateVerificationString());
 
     console.log("Message sent: %s", mailSent.messageId);
     return true;
