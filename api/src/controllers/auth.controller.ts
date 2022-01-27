3cd19643d9f842d180f8ba86fb303c7df59eb382
WilczekCK <pwilk0108@gmail.com>
2022-01-27 15:38:06 +0100

    nodemailer basic func to send mail created

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -5,13 +5,9 @@
     - User deletion
     - OAuth
 */
-
-/* TESTING PURPOSE */
-const testAccount = await nodemailer.createTestAccount();
-/* TESTING PURPOSE */
-
 import { getUsers } from './user.controller';
 import { compareData } from './hashing.controller';
+import { createTestAccount, createTransport } from 'nodemailer';
 
 type queryResults = {
     [where: string]: Record<string, string>
@@ -26,5 +22,24 @@ export const areCredentialsValid = async ({where: whereQuery}: queryResults): Pr
 }
 
 export const sendVerificationEmail = async (): Promise<boolean> => {
+    const testAccount = await createTestAccount();
+    const transporter = createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
+    })
 
+    const mailSent = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "wilkuwdr2008@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+    return true;
 }
