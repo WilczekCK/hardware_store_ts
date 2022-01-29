0e20d6d99e2acaa45204c27c7692d2b0e4d2ea28
WilczekCK <pwilk0108@gmail.com>
2022-01-29 17:24:22 +0100

    put route for forgotPassword

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -69,12 +69,13 @@ export const verifyUser = async ({where: whereQuery}: queryResults): Promise<boo
     return true;
 }
 
-export const sendForgotPasswordEmail = async (firstName:string, email: string, newPassword: string): Promise<boolean> => {
+export const sendForgotPasswordEmail = async ({where: whereQuery}: queryResults, newPassword: string): Promise<boolean> => {
     const transporter:Transporter = createTransport(config);
-
+    const firstName = 'test';
     const { html, to }:Record<string,string> = mails.forgotPassword;
+
     const mailSent:Record<string,number> = await transporter.sendMail({
-        to:   to.replace("[mail_to]", email),
+        to:   to.replace("[mail_to]", whereQuery.email),
         html: html.replace("[password]", newPassword)
                   .replace("[name]", firstName),
         ...mails,
--- api/src/routes/auth/put.ts
+++ api/src/routes/auth/put.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { generateVerificationString, sendForgotPasswordEmail } from "../../controllers/auth.controller";
 
 var router = express.Router();
 
 router.put('/', async (req, res) => {
   //
+})
+
+router.put('/forgotPassword', async (req, res) => {
+  const newPassword = generateVerificationString();
+  const isMailSent = await sendForgotPasswordEmail(req.body, newPassword);
 })
 
 
