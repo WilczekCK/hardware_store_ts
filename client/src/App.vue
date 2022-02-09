19879cc2dfea0aa18e2a774903095bf6035b0285
WilczekCK <pwilk0108@gmail.com>
2022-02-09 16:12:18 +0100

    sticky works + css for it

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
-van-row(id="nav" tag="nav" align="center")
-  van-col(span="4")
-    van-button(plain type="primary")="Logo"
-  van-col(span="20")
-    van-row(justify="end")
+nav#nav(ref="header_container")
+  van-sticky(:container="header_container")
+    van-row(align="center" class="header__container")
       van-col(span="4")
-        router-link(to="/")
-          ="Home"
-      van-col(span="4")
-        router-link(to="/about")
-          ="About"
+        van-button(plain type="primary")="Logo"
+      van-col(span="20")
+        van-row(justify="end")
+          van-col(span="4")
+            router-link(to="/")
+              ="Home"
+          van-col(span="4")
+            router-link(to="/about")
+              ="About"
 router-view
 </template>
 
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { Button, Row, Col, RowAlign, RowJustify } from "vant";
+import { Button, Row, Col, RowAlign, RowJustify, Sticky } from "vant";
 
 import App from "./App.vue";
 import router from "./router";
@@ -9,7 +9,7 @@ const app = createApp(App);
 app.config.performance = true; //enable vue troubleshooting
 
 /* VANT */
-app.use(Button).use(Row).use(Col);
+app.use(Button).use(Row).use(Col).use(Sticky);
 /* VANT */
 
 app.use(store).use(router).mount("#app");
