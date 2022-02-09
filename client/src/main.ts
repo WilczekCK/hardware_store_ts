1a64555190999cd1d34b7aec0eef06fbc5ed76df
WilczekCK <pwilk0108@gmail.com>
2022-02-09 12:31:42 +0100

    devtools for vue

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.config.performance = true; //enable vue troubleshooting
+
+app.use(store).use(router).mount("#app");
