a3755783f3e48fe7e871b4bd9b06512c44af0abc
WilczekCK <pwilk0108@gmail.com>
2022-02-10 16:54:00 +0100

    added param for route + created single auction page

---
+++ client/src/views/Auction.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.about
+  h1="This is an single auction page"
+  p {{auction.brand}} {{auction.series}} for {{auction.price}} PLN
+  p Added at {{auction.createdAt}}
+  p {{auction.description}}
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { useRoute } from "vue-router";
+import axios from "axios";
+
+interface serverResponse {
+  [key: string]: number | string | boolean;
+}
+
+export default class Auction extends Vue {
+  auction: serverResponse = {};
+  id: number;
+  
+  created() {
+    const route = useRoute();
+    this.id = parseInt(route.params.id[0]);
+
+    axios.get(`/auctions/${this.id}`).then(response => {
+      this.auction = response.data[0];
+    });
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
+
+
