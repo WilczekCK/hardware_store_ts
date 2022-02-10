2d89e15246634667174addadcd0f75aa9b2e1336
WilczekCK <pwilk0108@gmail.com>
2022-02-10 12:44:54 +0100

    added <script> and <style> for that page

--- client/src/views/Auctions.vue
+++ client/src/views/Auctions.vue
@@ -2,3 +2,14 @@
 .about
   h1="This is an auctions page"
 </template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+export default class Auctions extends Vue {
+}
+</script>
+
+<style lang="scss">
+</style>
+
+
