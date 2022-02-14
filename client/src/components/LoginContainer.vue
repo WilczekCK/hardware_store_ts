8ae82f457e93a8b3852bc4b2d990fd7c8675d529
WilczekCK <pwilk0108@gmail.com>
2022-02-14 15:26:39 +0100

    added code, added route, just an init commit

---
+++ client/src/components/LoginContainer.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+van-form(@submit="onSubmit")
+  van-cell-group(inset)
+    van-field(
+      v-model="username"
+      name="Username"
+      label="Username"
+      placeholder="Username"
+      :rules="[{ required: true, message: 'Username is required' }]")
+    van-field(
+      v-model="password"
+      type="password"
+      name="Password"
+      label="Password"
+      placeholder="Password"
+      :rules="[{ required: true, message: 'Password is required' }]")
+  div(style="margin: 16px;")
+    van-button(round block type="primary" native-type="submit")=Submit
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+export default class LoginContainer extends Vue {
+
+  onSubmit() {
+    return;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import "../scss/main.scss";
+</style>
+
+
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -34,6 +34,15 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Auction.vue"),
   },
+  {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/LoginContainer.vue"),
+  },
 ];
 
 const router = createRouter({
