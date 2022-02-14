909d7407376e553feea327d4e8a5e49b06e03bbd
WilczekCK <pwilk0108@gmail.com>
2022-02-14 14:15:10 +0100

    added sample html to insert user info in vue

--- client/src/views/Auction.vue
+++ client/src/views/Auction.vue
@@ -4,6 +4,8 @@ van-loading(type="spinner" v-if="!isLoaded")
   h1="This is an single auction page"
   p {{auction.brand}} {{auction.series}} for {{auction.price}} PLN
   p Added at {{auction.createdAt}}
+  p By: {{auction.user.firstName}}
+  p Mail: {{auction.user.email}}
   p {{auction.description}}
   van-button(:type="'success'" @click="$router.options.history.state.back ? $router.go(-1) : $router.push('/auctions')")
     = "Back to auctions"
@@ -30,9 +32,6 @@ export default class Auction extends Vue {
     axios.get(`/auctions/${this.id}`).then(response => {
       this.auction = response.data[0];
       this.isLoaded = true;
-
-
-      console.log(this.auction);
     });
   }
 }
