1d9e54fda41ec7ff90ccc3acaf6c209c52387758
WilczekCK <pwilk0108@gmail.com>
2022-03-18 15:20:12 +0100

    removed page which was just for testing purposees

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -43,15 +43,6 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../components/LoginContainer.vue"),
   },
-  {
-    path: "/authTester",
-    name: "AuthTester",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../components/authTester.vue"),
-  },
 ];
 
 const router = createRouter({
