b3da40c70e8661fb0c707f03d1c03b07d09a74f4
WilczekCK <pwilk0108@gmail.com>
2022-03-22 14:30:47 +0100

    global set of VANT to be strict english based

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
-import { Button, Row, Col, RowAlign, RowJustify, Sticky, NavBar, Loading, Form, Field, CellGroup, Toast, Picker, Checkbox, Popup, AddressEdit } from "vant";
+import { Button, Row, Col, RowAlign, RowJustify, Sticky, NavBar, Loading, Form, Field, CellGroup, Toast, Picker, Checkbox, Popup, AddressEdit, Locale } from "vant";
+import enUS from 'vant/es/locale/lang/en-US';
 import { globalCookiesConfig } from "vue3-cookies";
 
 import App from "./App.vue";
@@ -9,11 +10,15 @@ import axios from "axios";
 
 
 
+
+
+
 const app = createApp(App);
 app.config.performance = true; //enable vue troubleshooting
 
 /* VANT */
 app.use(Button).use(Row).use(Col).use(Sticky).use(NavBar).use(Loading).use(Form).use(Field).use(CellGroup).use(Picker).use(AddressEdit).use(Checkbox).use(Popup).use(Toast);
+Locale.use('en-US', enUS);
 
 /* AXIOS */
 axios.defaults.baseURL = "http://localhost:8000/";
