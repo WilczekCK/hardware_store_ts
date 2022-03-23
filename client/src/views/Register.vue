51ce5e5fa0734a6777cba2344f2a56b607a80163
WilczekCK <pwilk0108@gmail.com>
2022-03-23 13:58:52 +0100

    css + added form as container to display errors

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
-.register__container
-  h3( class="login__error" v-if="error") {{ error }}
+van-form(class="register__container")
+  h3( class="register__container__error" v-if="error") {{ error }}
   van-cell-group(inset)
     van-field(
       v-model="firstName"
+      name="firstName"
       label="First Name"
-      placeholder="First Name")
+      placeholder="First Name"
+      :rules="[{ required: true, message: 'First name is required' }]")
     van-field(
       v-model="email"
       name="email"
@@ -26,8 +28,8 @@
       label="Type password again"
       placeholder="Password"
       :rules="[{ required: true, message: 'Verify passport by writing it again' }]")
-    van-checkbox(v-model="rulesAccepted" @click="rulesAccepted = !!rulesAccepted")="I agree to the rules and conditions of the site"
-    van-checkbox(v-model="isAdult" @click="isAdult = !!isAdult")="I am at least 18 years old"
+    van-checkbox(class="register__container__checkbox" v-model="rulesAccepted" @click="rulesAccepted = !!rulesAccepted")="I agree to the rules and conditions of the site"
+    van-checkbox(class="register__container__checkbox" v-model="isAdult" @click="isAdult = !!isAdult")="I am at least 18 years old"
   div(style="margin: 16px;" class="login__container__submit__container")
     van-button(block type="success" native-type="submit")="Submit"
 </template>
@@ -52,9 +54,17 @@ export default class Register extends Vue {
 </script>
 
 <style lang="scss">
+@import "../scss/main.scss";
 .register__container{
     max-width: 350px;
     margin:0 auto;
+    .register__container__error{ color:red }
+    .register__container__checkbox{
+        padding:15px 0px;
+        text-align:left;
+        width: 90%;
+        margin-left:20px;
+    }
 }
 </style>
 
