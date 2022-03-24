b640103823fd33a44b3235fbd5bb06165f73dc18
WilczekCK <pwilk0108@gmail.com>
2022-03-24 14:13:17 +0100

    proper behave or verification, we do noot the additional function

--- api/src/routes/auth/patch.ts
+++ api/src/routes/auth/patch.ts
@@ -11,14 +11,7 @@ router.patch('/verify', async (req, res) => {
   const getUserIdByCode = await getUserByVerificationCode(req.body);
   if ( !getUserIdByCode ) return res.send( {status: 202, message: `No verification code like this or user is verified`} );
 
-  const isVerified = await verifyUser({ 
-      where: {
-        verificationCode: req.body.where.verificationCode,
-        id: getUserIdByCode
-      }
-    });
-  if ( !isVerified ) return res.send( {status: 202, message: `No user like that, wrong verification code`} );
-
+  await verifyUser({ where: { verificationCode: req.body.where.verificationCode, id: getUserIdByCode } });
   return res.send( {status: 200, message: `User verified`} );
 })
 
