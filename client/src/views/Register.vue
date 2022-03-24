31aa53196121045853b4831ab24c8d108c4a9077
WilczekCK <pwilk0108@gmail.com>
2022-03-24 12:29:03 +0100

    after successfuly created user, show other box with link to home

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,37 +1,43 @@
 <template lang="pug">
-van-form(class="register__container" @submit="onSubmit")
-  h3( class="register__container__error" v-if="errors" v-for="error in errors") {{ error }}
-  van-cell-group(inset)
-    van-field(
-      v-model="firstName"
-      name="firstName"
-      label="First Name"
-      placeholder="First Name"
-      :rules="[{ required: true, message: 'First name is required' }]")
-    van-field(
-      v-model="email"
-      name="email"
-      label="Email"
-      placeholder="Email"
-      :rules="[{ required: true, message: 'Email is required' }]")
-    van-field(
-      v-model="password"
-      type="password"
-      name="password"
-      label="Password"
-      placeholder="Password"
-      :rules="[{ required: true, message: 'Password is required' }]")
-    van-field(
-      v-model="verifyPassword"
-      type="password"
-      name="check_password"
-      label="Type password again"
-      placeholder="Password"
-      :rules="[{ required: true, message: 'Verify passport by writing it again' }]")
-    van-checkbox(class="register__container__checkbox" v-model="rulesAccepted" @click="rulesAccepted = !!rulesAccepted"  :rules="[{ required: true, message: 'You have to accept rules' }]")="I agree to the rules and conditions of the site"
-    van-checkbox(class="register__container__checkbox" v-model="isAdult" @click="isAdult = !!isAdult"  :rules="[{ required: true, message: 'You have to be adult' }]")="I am at least 18 years old"
-  div(style="margin: 16px;" class="login__container__submit__container")
-    van-button(block type="success" native-type="submit")="Submit"
+.register__container
+  van-form(@submit="onSubmit" v-if="!isCreatedSuccessfully")
+    h3( class="register__container__error" v-if="errors" v-for="error in errors") {{ error }}
+    van-cell-group(inset)
+      van-field(
+        v-model="firstName"
+        name="firstName"
+        label="First Name"
+        placeholder="First Name"
+        :rules="[{ required: true, message: 'First name is required' }]")
+      van-field(
+        v-model="email"
+        name="email"
+        label="Email"
+        placeholder="Email"
+        :rules="[{ required: true, message: 'Email is required' }]")
+      van-field(
+        v-model="password"
+        type="password"
+        name="password"
+        label="Password"
+        placeholder="Password"
+        :rules="[{ required: true, message: 'Password is required' }]")
+      van-field(
+        v-model="verifyPassword"
+        type="password"
+        name="check_password"
+        label="Type password again"
+        placeholder="Password"
+        :rules="[{ required: true, message: 'Verify passport by writing it again' }]")
+      van-checkbox(class="register__container__checkbox" v-model="rulesAccepted" @click="rulesAccepted = !!rulesAccepted"  :rules="[{ required: true, message: 'You have to accept rules' }]")="I agree to the rules and conditions of the site"
+      van-checkbox(class="register__container__checkbox" v-model="isAdult" @click="isAdult = !!isAdult"  :rules="[{ required: true, message: 'You have to be adult' }]")="I am at least 18 years old"
+    div(style="margin: 16px;" class="login__container__submit__container")
+      van-button(block type="success" native-type="submit")="Submit"
+  div(v-else)
+    h3="You have successfully created an account"
+    p="Check the mail for the confirmation link"
+    router-link(to="/")
+      van-button(block type="success" native-type="submit" href="/")="Back to homepage"
 </template>
 
 <script lang="ts">
@@ -46,6 +52,7 @@ interface createUserResponse {
 export default class Register extends Vue {
     rulesAccepted = false;
     isAdult = false;
+    isCreatedSuccessfully = false;
 
     firstName = '';
     email = '';
@@ -80,8 +87,10 @@ export default class Register extends Vue {
       const { message, status } = await this.sendUserInfo();
         
       if( status !== 200 ) {
-        this.errors.push(message);
+        return this.errors.push(message);
       }
+
+      this.isCreatedSuccessfully = true;
     }
   }
 }
