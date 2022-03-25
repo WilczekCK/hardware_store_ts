f35cd852fd237a368e5a6b2e4fe1c8fd74d2c4e2
WilczekCK <pwilk0108@gmail.com>
2022-03-25 11:50:46 +0100

    inputs inserted for config profile

--- client/src/views/ConfigProfile.vue
+++ client/src/views/ConfigProfile.vue
@@ -1,5 +1,27 @@
 <template lang="pug">
 .profile__container
+ van-form(@submit="onSubmit")
+    h3( class="register__container__error" v-if="errors" v-for="error in errors") {{ error }}
+    van-cell-group(inset)
+        van-field(
+            v-model="firstName"
+            name="firstName"
+            label="First Name"
+            placeholder="First Name"
+            :rules="[{ required: true, message: 'First name is required' }]")
+        van-field(
+            v-model="lastName"
+            name="lastName"
+            label="Last Name"
+            placeholder="Last Name"
+            :rules="[{ required: true, message: 'Last name is required' }]")
+        van-field(
+            v-model="city"
+            name="city"
+            label="City"
+            placeholder="City")
+    div(style="margin: 16px;" class="login__container__submit__container")
+      van-button(block type="success" native-type="submit")="Change"
 </template>
 
 <script lang="ts">
@@ -8,7 +30,13 @@ import axios, { AxiosResponse } from "axios";
 
 
 export default class CutomizeProfile extends Vue {
+    firstName = '';
+    lastName = '';
+    city = '';
 
+    onSubmit() {
+        return true;
+    }
 }
   
 </script>
