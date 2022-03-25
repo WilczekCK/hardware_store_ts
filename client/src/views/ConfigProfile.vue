1e720e909aa75f89c480d404f5e566e1cab359c3
WilczekCK <pwilk0108@gmail.com>
2022-03-25 12:39:41 +0100

    preview profile link available

--- client/src/views/ConfigProfile.vue
+++ client/src/views/ConfigProfile.vue
@@ -4,7 +4,7 @@
     h3( class="register__container__error" v-if="errors" v-for="error in errors") {{ error }}
     
     van-uploader(:after-read="afterRead")
-
+ 
     van-cell-group(inset)
         van-field(
             v-model="firstName"
@@ -23,19 +23,30 @@
             name="city"
             label="City"
             placeholder="City")
-    div(style="margin: 16px;" class="login__container__submit__container")
-      van-button(block type="success" native-type="submit")="Change"
+    div(style="margin: 16px;")
+      van-button(block type="success" native-type="submit")="Save changes"
+      router-link( :to="`/profile/${userId}`" target="_blank")
+        van-button(block class="profile__container__preview__button" type="default" )="Preview profile"
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
 import axios, { AxiosResponse } from "axios";
+import { useStore } from 'vuex';
 
 
 export default class CutomizeProfile extends Vue {
+    store = useStore();
+
+    userId = 1; 
     firstName = '';
     lastName = '';
     city = '';
+
+
+    async created(){
+        this.userId = this.store.getters.getUserId;
+    }
 
     afterRead = (file: string) => {
       console.log(file);
@@ -53,6 +64,10 @@ export default class CutomizeProfile extends Vue {
 .profile__container{
     max-width: 350px;
     margin:0 auto;
+
+    .profile__container__preview__button{
+        margin-top: 16px;
+    }
 }
 </style>
 
