7ad7207bf96b7ce77f944e6fac5f01a5c8829e8d
WilczekCK <pwilk0108@gmail.com>
2022-03-31 16:11:14 +0200

    displaying first name of user + force int for params

--- client/src/views/VisitProfile.vue
+++ client/src/views/VisitProfile.vue
@@ -1,13 +1,41 @@
 <template lang="pug">
 .profile__container
+    h1 {{ firstName }}
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
 import axios, { AxiosResponse } from "axios";
 
-
 export default class VisitProfile extends Vue {
+    userId = 0; 
+    firstName:any = '';
+    lastName:any = '';
+    city = '';
+
+    async beforeMount(){
+        this.userId =+ this.$route.params.id //force convert to int
+
+        await this.fetchInformationsAboutProfile();
+    }
+
+    async fetchInformationsAboutProfile(){
+        const { data, status } = await axios.get('/accounts/'+this.userId )
+            .then((response: AxiosResponse) => {
+                return response;
+            })
+            .catch((error) => {
+                return { status: 201, data: 'No user like that' };
+            });
+
+        if(status === 200){
+            const { firstName, lastName, city } = data[0];
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            //this.city = data.city;
+        }
+    }
 
 }
   
@@ -16,8 +44,6 @@ export default class VisitProfile extends Vue {
 <style lang="scss">
 @import "../scss/main.scss";
 .profile__container{
-    max-width: 350px;
-    margin:0 auto;
 }
 </style>
 
