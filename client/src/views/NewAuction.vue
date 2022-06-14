eda37881b5ca7312725383fb87388ca74b66745b
Paweł Wilk <pwilk0108@gmail.com>
2022-06-14 14:19:15 +0200

    fields inserted, for now just a text field

--- client/src/views/NewAuction.vue
+++ client/src/views/NewAuction.vue
@@ -4,12 +4,41 @@
     h3( class="register__container__error" v-if="errors" v-for="error in errors") {{ error }}
     van-form()
         van-cell-group(inset)
-            van-divider="Product informations"
+            van-divider="Product information"
             van-field(
-                v-model="productName"
-                name="productName"
-                label="Name of the product"
+                v-model="brand"
+                name="brand"
+                label="Brand"
+                placeholder="AMD, Intel, etc."
                 :rules="[{ required: true }]"
+            )
+            van-field(
+                v-model="series"
+                name="series"
+                label="Series"
+                placeholder="RTX, GTX, etc."
+                :rules="[{ required: true }]"
+            )
+            van-field(
+                v-model="price"
+                type="digit"
+                name="price"
+                label="Price"
+                placeholder="250"
+                :rules="[{ required: true }]"
+            )
+                template(#button)
+                    ="PLN"
+            van-field(
+                v-model="description"
+                rows="2"
+                autosize
+                name="description"
+                label="Description"
+                type="textarea"
+                maxlength="250"
+                placeholder="Some information about the product"
+                show-word-limit
             )
         div(style="margin: 16px;")
         van-button(block type="success" native-type="submit")="Create auction!"
@@ -21,8 +50,10 @@ import { Vue } from "vue-class-component";
 import axios from "axios";
 
 export default class NewAuction extends Vue {
-    productName = '';
-
+    brand = '';
+    series = '';
+    description = '';
+    price?:number;
 }
 </script>
 
@@ -30,7 +61,7 @@ export default class NewAuction extends Vue {
 .auction__create__container{
     max-width: 350px;
     margin: 0 auto;
-    
+
     small { opacity:0.8; font-size: 11px; padding-top:5px; }
 }
 </style>
